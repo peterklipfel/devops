33f379cb3cf135b3b6d5b068f8f7584e71e14de6
Peter Klipfel <peter@klipfel.me>
2013-07-14 11:34:28 -0600

    vim > nano

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 sudo apt-get update
 sudo apt-get upgrade
 
-sudo apt-get install -y build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison pkg-config tree aptitude
+sudo apt-get install -y build-essential openssl vim libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison pkg-config tree aptitude
 
 
 
