15d9bfcc1dfc0c4e6fac02a5874e9b49c991f53a
Peter Klipfel <peter@klipfel.me>
2013-07-14 11:12:30 -0600

    simple bootstrapping script

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison pkg-config tree aptitude
+
+`
+
