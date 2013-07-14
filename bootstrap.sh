ca0a477d715624d2ddd651f760d0e711f9fa6de8
Peter Klipfel <peter@klipfel.me>
2013-07-14 11:17:46 -0600

    removed rogue character

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,5 @@ sudo apt-get upgrade
 
 sudo apt-get install -y build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison pkg-config tree aptitude
 
-`
+
 
